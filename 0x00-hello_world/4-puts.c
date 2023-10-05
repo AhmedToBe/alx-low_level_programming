bd117abd1ede3d9a8f2596d000e368240538708b
AhmedToBe <drmaster.net@gmail.com>
2023-10-05 19:59:25 +0300

    asdfa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,5 @@
-#include <stdio.h>
-/**
- *  * main - ssss
- *   *
- *    * Description: sssss
- *     *
- *      * Return: ssssss
- *      */
 int main(void)
 {
-
-	puts("\"Programming is like building a multilingual puziie");
-
+	puts("\"Programming is like building a multilingual puziie);
 	return (0);
-
 }
