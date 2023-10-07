d702fc7190f2ee10d14284365a63e6dd549b800f
AhmedToBe <drmaster.net@gmail.com>
2023-10-08 02:17:32 +0300

    asdfasdf

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,6 +13,7 @@ int main(void)
 	for (i = 0; j <= 99; i++)
 	{
 		for (j = 0; j <= 99; j++)
+		{
 			if (i < j && i != j)
 			{
 				putchar((i / 10) + '0');
@@ -21,11 +22,13 @@ int main(void)
 				putchar((j / 10) + '0');
 				putchar((j % 10) + '0');
 				if (i != 98 || j != 99)
+			{
+			putchar(',');
+			putchar(' ');
 			}
-		putchar(',');
-		putchar(' ');
+			}
+		}
 	}
-}
 putchar('\n')
 return (0);
 }
