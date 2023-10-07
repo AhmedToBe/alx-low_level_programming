e6cc3c3de8182e6b67122a0e56bdf810a609c71d
AhmedToBe <drmaster.net@gmail.com>
2023-10-07 21:45:45 +0300

    adfds

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - finction main is the function.
+ *
+ * Return: always (0)
+ **/
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	return (0);
+}
