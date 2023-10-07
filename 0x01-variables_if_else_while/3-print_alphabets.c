24f5c7c087bb8e1487b790f3f4d5ff51c81225e2
AhmedToBe <drmaster.net@gmail.com>
2023-10-07 22:25:40 +0300

    asdfasdd4df

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,24 @@
 int main(void)
 
 {
-char alp[] = "abcdefghijklmnopqrstuvwxyz";
-for (int i = 0; alp[i] != '\0'; i++)
-	{
-	printf("%c ", alp[i]);
-	}
-	printf("\n");
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z')
+		{
+			putchar (d);
+			d++
+		}
+	putchar ('\n');
 	return (0);
 }
