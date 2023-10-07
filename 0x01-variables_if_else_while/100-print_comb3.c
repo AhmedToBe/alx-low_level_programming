0e9390f38a3bdbc740cf1f5953257b1a9537a16a
AhmedToBe <drmaster.net@gmail.com>
2023-10-08 02:56:54 +0300

    adfadsf

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 /**
- *  * main - prog Entry point
- *   * Return: Always 0 (Success)
- *    */
-int main (void)
+ * main - prog Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = '0';
+	int j = '0';
 
+	while (i <= 9)
+	{
+		while (j <= '9')
+		{
+			if (!(i > j) || i == j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '0' && j == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putcahr(' ');
+				}
+			}
+			j++
+		}
+		j = '0';
+		i++;
+	}
+return (0);
+}
