26c9442963ace727ed144e6c780660c003197f20
AhmedToBe <drmaster.net@gmail.com>
2023-10-10 23:39:19 +0300

    adfasdf

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _abc - function that computes the absolute
+ * _abs - function that computes the absolute
  *	value of an integer
  *
  * @n: takes in integer type input for function
@@ -9,7 +9,7 @@
  * Return: Always 0 (Success)
  */
 
-int _abc(int n)
+int _abs(int n)
 {
 	if (n < 0)
 		n = (-1) * n;
