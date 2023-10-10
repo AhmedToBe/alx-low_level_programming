3125b78f4915a22919c155480d94aade54723d7d
AhmedToBe <drmaster.net@gmail.com>
2023-10-11 01:23:55 +0300

    dfdf

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: print the first 50 Fibonancci numbers
+ *	starting with 1 and 2 folled by a new line
+ *
+ *	Return: Always 0 success
+ */
+
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sun;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib 2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+
+{
